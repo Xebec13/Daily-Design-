a6d118182d31624b383bdb317e566d958cb8b7fb
David <dhoesen@gmail.com>
2024-12-11 14:03:50 +0100

    100%

---
+++ 36 Purple Msg/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const box1 = document.querySelector('.container');
+    const box2 = document.querySelector('.msg-chat');
+  
+    
+    box2.style.height = `${box1.offsetHeight - 60}px`;
+  };
